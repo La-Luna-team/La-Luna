e070d474611b2b2a79b84561ea6e2b4cf40bbdd1
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-02-26 15:57:39 +0900

    [Feat] 공통 레이아웃 - 헤더

---
+++ src/main/java/com/laluna/laluna/controller/SampleController.java
@@ -0,0 +1,14 @@
+package com.laluna.laluna.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/test")
+    public String showTest() {
+        return "home/home";
+    }
+}
