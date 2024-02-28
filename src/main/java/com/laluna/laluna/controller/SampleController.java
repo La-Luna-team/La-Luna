f8d4833ddf3847e85bf7074fac1a422aa77d66f5
yong <sy99639@naver.com>
2024-02-28 19:00:38 +0900

    [feat] view/게시판조회

--- src/main/java/com/laluna/laluna/controller/SampleController.java
+++ src/main/java/com/laluna/laluna/controller/SampleController.java
@@ -1,7 +1,6 @@
 package com.laluna.laluna.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -9,6 +8,6 @@ public class SampleController {
 
     @GetMapping("/test")
     public String showTest() {
-        return "home/home";
+        return "home/register";
     }
 }
