af6c7c5c12a39293d9118aaa2d2735fa40d0001d
DESKTOP-D524MI3\804-07 <bae4445@gmail.com>
2024-03-05 18:50:42 +0900

    [Feat]업무/게시글-조회 완료

--- src/main/java/com/laluna/laluna/config/MyUserDetails.java
+++ src/main/java/com/laluna/laluna/config/MyUserDetails.java
@@ -1,12 +1,10 @@
 package com.laluna.laluna.config;
 
-import com.laluna.laluna.domain.entity.Pets;
 import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
-import java.util.List;
 
 @Getter
 public class MyUserDetails extends User {
