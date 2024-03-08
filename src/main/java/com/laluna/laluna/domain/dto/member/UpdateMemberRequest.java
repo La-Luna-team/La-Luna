fe82d26b38bd0925eea9775dc711bc84ae7fb7d4
ganggwonlee@gmail.com <ganggwonlee@gmail.com>
2024-03-08 18:04:46 +0900

    [feat] update

---
+++ src/main/java/com/laluna/laluna/domain/dto/member/UpdateMemberRequest.java
@@ -0,0 +1,19 @@
+//package com.laluna.laluna.domain.dto.member;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Setter
+//public class UpdateMemberRequest {
+//
+//    private String memberid;
+//    private String memberpassword;
+//    private String phone;
+//    private String address;
+//    private String email;
+//}
---
+++ src/main/java/com/laluna/laluna/domain/dto/member/UpdateMemberResponse.java
@@ -0,0 +1,19 @@
+//package com.laluna.laluna.domain.dto.member;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class UpdateMemberResponse {
+//
+//    private Long memberno;
+//    private String memberid;
+//    private String memberpassword;
+//    private String roles;
+//    private String phone;
+//    private String address;
+//    private String email;
+//}
